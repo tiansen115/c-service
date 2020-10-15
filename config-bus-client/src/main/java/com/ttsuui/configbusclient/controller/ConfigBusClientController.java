30a534a407fe3db258683617b780a26d5b78788d
tiansen115 <tiansen115@163.com>
2020-10-15 09:33:14 +0800

    配置模块

---
+++ config-bus-client/src/main/java/com/ttsuui/configbusclient/controller/ConfigBusClientController.java
@@ -0,0 +1,16 @@
+package com.ttsuui.configbusclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigBusClientController {
+    @Value("${neo.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
